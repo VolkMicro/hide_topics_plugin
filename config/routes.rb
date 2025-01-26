485d7d64564da191b9fbac37a5cf2c49bf66c88f
aleksandr.khlebnikov <aleksandr.khlebnikov@wirenboard.com>
2025-01-26 21:15:43 +0300

    Initial commit for hide_topics_plugin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 MyPluginModule::Engine.routes.draw do
-  get "/examples" => "examples#index"
-  # define routes here
+  get "/test" => "test#index"
 end
 
-Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "my-plugin" }
+Discourse::Application.routes.draw { mount ::MyPluginModule::Engine, at: "/hide_topics" }
--- plugin.rb
+++ plugin.rb
@@ -1,21 +1,45 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
-# about: TODO
-# meta_topic_id: TODO
-# version: 0.0.1
-# authors: Discourse
-# url: TODO
+# name: hide_topics
+# about: Automatically hide topics based on categories with flexible visibility settings
+# version: 0.1
+# authors: MicroVolk
+# url: https://github.com/VolkMicro/hide_topics_plugin
 # required_version: 2.7.0
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :hide_topics_enabled
 
 module ::MyPluginModule
-  PLUGIN_NAME = "discourse-plugin-name"
+  PLUGIN_NAME = "hide_topics"
+
+  class Engine < ::Rails::Engine
+    engine_name PLUGIN_NAME
+    isolate_namespace MyPluginModule
+  end
 end
 
 require_relative "lib/my_plugin_module/engine"
 
 after_initialize do
-  # Code which should run after Rails has finished booting
+  # Hide topics when created in specified categories
+  DiscourseEvent.on(:topic_created) do |topic|
+    if SiteSetting.hide_topics_enabled && SiteSetting.hide_topics_categories.include?(topic.category.slug)
+      topic.update_status('hidden', true)
+      Rails.logger.info("Topic #{topic.id} hidden by hide_topics plugin in category #{topic.category.slug}.")
+    end
+  end
+
+  # Helper to scan and hide existing topics
+  def self.hide_existing_topics
+    Category.where(slug: SiteSetting.hide_topics_categories).each do |category|
+      category.topics.each do |topic|
+        next if topic.hidden?
+        topic.update_status('hidden', true)
+        Rails.logger.info("Existing topic #{topic.id} hidden in category #{category.slug}.")
+      end
+    end
+  end
+
+  # Ensure the scanning occurs on plugin activation
+  hide_existing_topics if SiteSetting.hide_topics_enabled
 end
